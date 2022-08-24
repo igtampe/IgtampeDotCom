07186cc563f643052c2fc4f91908e906fbfb7e2c
Ignacio Tampe <Igtampe@gmail.com>
2022-08-24 01:03:06 -0400

    ImageCard components for displaying images

---
+++ igtampedotcom/src/components/imagecard/ImageCard.jsx
@@ -0,0 +1,37 @@
+import { Card, Divider, Link, Typography } from "@mui/material";
+
+
+export default function ImageCard({
+    imgSrc = '', title = '', children, href = ""
+}) {
+
+    return (
+        <Card>
+            {
+                href === ""
+                    ? <img style={{ width: '100%', height: '100%', maxHeight: '200px', objectFit: 'cover' }} src={imgSrc} />
+                    : <a href={href}><img style={{ width: '100%', height: '100%', maxHeight: '200px', objectFit: 'cover' }} src={imgSrc} /></a>
+            }
+            {
+                (title !== '' || children !== undefined) && <div style={{padding:'15px'}}>
+                    {
+                        title !== '' && <>
+                            {
+                                href !== ""
+                                    ? <Link href={href}><Typography fontFamily={'orbitron'} fontWeight='200'>{title}</Typography></Link>
+                                    : <Typography fontFamily={'orbitron'} fontWeight='200'>{title}</Typography>
+                            }
+                            <Divider style={{ marginBottom: '10px' }} />
+                        </>
+                    }
+                    {
+                        children !== undefined && <> {children}</>
+                    }
+
+                </div>
+
+            }
+        </Card>
+    )
+
+}
