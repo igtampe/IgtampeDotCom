9b16edc46b114d68c60e9f46b90699f22e5250a3
Ignacio Tampe (Chopo) <Igtampe@gmail.com>
2022-11-22 11:37:39 -0400

    Fix missing image

--- igtampedotcom/src/components/pages/programs/ired/IredPage.jsx
+++ igtampedotcom/src/components/pages/programs/ired/IredPage.jsx
@@ -27,7 +27,7 @@ export default function IredPage({
                 </ul>
             </ElevatedSection>
         </ImageSideSection>
-        <ImageSideSection Vertical={Vertical} imgSrc="http://localhost:3000/images/programs/neco/main.png">
+        <ImageSideSection Vertical={Vertical} imgSrc="/images/programs/neco/main.png">
             <ElevatedSection title="Trusted Base">
                 IRED/IDACRA is based on practices and tools that have been in development since Neco, and continue to power it, Atlas, and other minor projects.
                 Their reusability and adaptability have only been accentuated with their release as NuGet packages available now!
